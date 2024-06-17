24a8a7f7c1d55c7df5ff0e7e2abb56e2c478333a
Tommy Webb <tommy@calyxinstitute.org>
2024-06-17 17:18:23 +0000

    Redact location for pre-T target SDK too
    Change-Id: I9e5bea0e416c1d958223102a0be9eeeddfc42bf1

--- src/com/android/providers/media/util/PermissionUtils.java
+++ src/com/android/providers/media/util/PermissionUtils.java
@@ -154,10 +154,11 @@ public class PermissionUtils {
     public static boolean checkPermissionAccessMediaLocation(@NonNull Context context, int pid,
             int uid, @NonNull String packageName, @Nullable String attributionTag,
             boolean isTargetSdkAtLeastT) {
+        // We lie about target SDK because lower SDK should not be rewarded with access to location.
         return checkPermissionForDataDelivery(context, ACCESS_MEDIA_LOCATION, pid, uid, packageName,
                 attributionTag, generateAppOpMessage(packageName, sOpDescription.get()))
                 || checkPermissionAccessMediaCompatGrant(context, pid, uid, packageName,
-                attributionTag, isTargetSdkAtLeastT);
+                attributionTag, /* isTargetSdkAtLeastT */ true);
     }
 
     /**
