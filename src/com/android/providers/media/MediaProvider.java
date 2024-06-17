5bf8cbc550f3913647df0148c0011acaf08feff9
Tommy Webb <tommy@calyxinstitute.org>
2024-06-17 17:18:23 +0000

    Redact location exif for older or legacy apps
    When strict location redaction is enabled, do not grant special
    treatment to apps with a pre-T target SDK or that "require" legacy
    storage access; such apps should have their location metadata redacted,
    too.
    
    Test: Manual: Try to view location in Details of LeafPic Revived v0.8.7,
    and the location should be zeroed out. (It may need to be a new file or
    else the app may need data cleared.) Share to Phone Saver 5.2.1, and the
    saved file should have zeroed-out metadata.
    
    Issue: calyxos#2468
    Change-Id: I9e5bea0e416c1d958223102a0be9eeeddfc42bf1

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -11010,9 +11010,15 @@ public class MediaProvider extends ContentProvider {
      * </ul>
      */
     private boolean shouldBypassFuseRestrictions(boolean forWrite, String filePath) {
+        return shouldBypassFuseRestrictions(forWrite, filePath, /* allowLegacy */ true);
+    }
+    private boolean shouldBypassFuseRestrictions(boolean forWrite, String filePath,
+            boolean allowLegacy) {
         boolean isRequestingLegacyStorage = forWrite ? isCallingPackageLegacyWrite()
                 : isCallingPackageLegacyRead();
-        if (isRequestingLegacyStorage) {
+        final boolean shouldAllowLegacy = StrictLocationRedactionHelper.getInstance(getContext())
+                .isSettingEnabled() ? allowLegacy : true;
+        if (allowLegacy && isRequestingLegacyStorage) {
             return true;
         }
 
@@ -11188,7 +11194,8 @@ public class MediaProvider extends ContentProvider {
                 clearLocalCallingIdentity(getCachedCallingIdentityForFuse(uid));
         try {
             if (!isRedactionNeeded()
-                    || shouldBypassFuseRestrictions(/* forWrite */ false, path)) {
+                    || shouldBypassFuseRestrictions(/* forWrite */ false, path,
+                            /* allowLegacy */ false)) {
                 return new long[0];
             }
 
