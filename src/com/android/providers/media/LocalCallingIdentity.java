0f828fbadef54d3a35d6a41b0b869608ff269476
Tommy Webb <tommy@calyxinstitute.org>
2024-06-18 18:15:20 +0000

    Redact location exif more often
    Enact strict location redaction when the STRICT_LOCATION_REDACTION
    secure setting is enabled or unset.
    
    Check permissions of the receiving/opening app, regardless of how it
    accesses the file, e.g. standard file access, intent, or picker.
    Redact location exif if the app does not meet our new access standards:
    - If the app has "All files access" or is the system gallery, allow.
    - If the app would have had access to the metadata prior to this change,
      and it currently has "All files access" or "Media management"
      permission, allow.
    - In all other cases, redact.
    
    Previously, a large number of cases were affected by the fact that
    redaction would never occur when:
    - The share source or content provider had "All files access".
    - The share source or content provider was the system gallery.
    - The app was accessing the files via the "Photos and videos" or "Files"
      permissions.
    
    Because the ACCESS_MEDIA_LOCATION permission is granted too often and
    cannot be revoked in a user-facing manner, this change considers it to
    remain a necessary but not sufficient permission for our purposes.
    
    This change resolves described problems for non-legacy apps; such apps
    are addressed separately via I9e5bea0e416c1d958223102a0be9eeeddfc42bf1.
    
    Test: Manual: Open Chromium. Upload an image with exif location metadata
    to a file hosting site, e.g. share.riseup.net. Download the image on
    another device. Verify that the sensitive metadata is absent.
    
    Issue: calyxos#2468
    Issue: calyxos#2472
    Reported-by: lucasmz
    Change-Id: Ic6bb59a32fe9441ecc8488c97c80e1ecbb51d5a1

--- src/com/android/providers/media/LocalCallingIdentity.java
+++ src/com/android/providers/media/LocalCallingIdentity.java
@@ -27,6 +27,7 @@ import static com.android.providers.media.util.PermissionUtils.checkPermissionAc
 import static com.android.providers.media.util.PermissionUtils.checkPermissionDelegator;
 import static com.android.providers.media.util.PermissionUtils.checkPermissionInstallPackages;
 import static com.android.providers.media.util.PermissionUtils.checkPermissionManageDocuments;
+import static com.android.providers.media.util.PermissionUtils.checkPermissionManageMedia;
 import static com.android.providers.media.util.PermissionUtils.checkPermissionManager;
 import static com.android.providers.media.util.PermissionUtils.checkPermissionQueryAllPackages;
 import static com.android.providers.media.util.PermissionUtils.checkPermissionReadAudio;
@@ -516,6 +517,10 @@ public class LocalCallingIdentity {
             default:
                 return false;
         }
+    }
+
+    public boolean hasManageMediaPermission() {
+        return checkPermissionManageMedia(context, pid, uid, getPackageName(), attributionTag);
     }
 
     private boolean isLegacyStorageGranted() {
